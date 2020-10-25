894c8e0a7e1b6e60d93b674e906c0d5bcdc43b96
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-25 13:13:38 +0530

    Started displaying image of the first result

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ form.addEventListener ('submit', async e =>
 {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const image = document.createElement("img");
+    image.src = res.data[0].show.image.medium;
+    document.body.append(image);
 })
