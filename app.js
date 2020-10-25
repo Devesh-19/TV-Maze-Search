eb9cd9ca5cca87d27eead6e1061ebe4daad9f4a2
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-25 13:22:21 +0530

    Displaying images for all the results

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ form.addEventListener ('submit', async e =>
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    const image = document.createElement("img");
-    image.src = res.data[0].show.image.medium;
-    document.body.append(image);
+    displayImages(res.data);
 })
+
+const displayImages = shows =>
+{
+    for (let result of shows)
+    {        
+        if (result.show.image)
+        {
+            const image = document.createElement("img");
+            image.src = result.show.image.medium;
+            document.body.append(image);
+        }
+    }
+}
