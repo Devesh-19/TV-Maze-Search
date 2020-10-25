7051c95d7db965c8fcc93761e16ef14201b95579
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-25 13:31:20 +0530

    Started removing previous search value and search results

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const form = document.querySelector("#searchForm");
+const images = [];
 
 form.addEventListener ('submit', async e =>
 {
     e.preventDefault();
+    for (let i = images.length - 1; i >= 0; i--)
+    {
+        images[i].src = ' ';
+        images.pop();
+    }
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
     displayImages(res.data);
+    form.elements.query.value = '';
 })
 
 const displayImages = shows =>
@@ -17,6 +25,7 @@ const displayImages = shows =>
             const image = document.createElement("img");
             image.src = result.show.image.medium;
             document.body.append(image);
+            images.push(image);
         }
     }
 }
